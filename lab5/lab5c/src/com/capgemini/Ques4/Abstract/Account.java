35e183f7216009fc6896c31668f77d76c25e8dd4
ShubhanshuTiwari <47036297+ShubhanshuTiwari@users.noreply.github.com>
2019-02-03 23:33:26 +0530

    Add files via upload

---
+++ lab5/lab5c/src/com/capgemini/Ques4/Abstract/Account.java
@@ -0,0 +1,7 @@
+package com.capgemini.Ques4.Abstract;
+
+public abstract class Account {
+ public abstract boolean withdraw(double balance);
+
+ 
+}
---
+++ lab5/lab5c/src/com/capgemini/Ques4/Account.java
@@ -0,0 +1,39 @@
+package com.capgemini.Ques4;
+
+public class Account extends com.capgemini.Ques4.Abstract.Account {
+long accountNum;
+double balance;
+Person accHolder;
+
+public Account( long accountNum,double balance, Person accHolder) {
+	
+	super();
+	this.balance = balance;
+	this.accHolder = accHolder;
+	this.accountNum=accountNum;
+	
+}
+
+
+public double getBalance() {
+	return balance;
+}
+
+
+public void deposit(double balance) {
+	this.balance+=balance;
+}
+
+public boolean withdraw(double balance) {
+	
+	this.balance-=balance;
+	
+	return true;
+}
+
+
+@Override
+public String toString() {
+	return "\nAccountNum -> "+accountNum+ "\nAccount holder Name->"+accHolder.getName()+"\n Accountholder Age->"+accHolder.getAge()+ "\nbalance->" + balance ;
+}
+}
